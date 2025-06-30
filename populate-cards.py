64c9b5a961ecf52877d5809428e49136e4f5a78d
Marcel Nunez <mpnunez28@gmail.com>
2025-06-29 18:45:33 -0700

    refactor

--- populate-cards.py
+++ populate-cards.py
@@ -3,14 +3,32 @@ import boto3
 from tqdm import tqdm
 
 ALL_CARDS_URL = "https://ringsdb.com/api/public/cards/"
-DDB_TABLE_NAME = "AwsLotrStack-PlayerCardsEAD479B2-QCT1UV83CK4J"
+ALL_PACKS_URL = "https://ringsdb.com/api/public/packs/"
 
-def main():
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+dynamodb_client = boto3.client('dynamodb')
+table_names = dynamodb_client.list_tables()['TableNames']
+
+def get_table_containing(name: str):
+    table_name = filter(lambda x: name in x, table_names)[0]
+    table = dynamodb.Table(table_name)
+    return table
+
+def populate_player_cards():
     cards = get_cards()
     print("Got cards")
 
-    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-    table = dynamodb.Table(DDB_TABLE_NAME)
+    table = get_table_containing("PlayerCards")
+
+    for card in tqdm(cards):
+        table.put_item(Item=card)
+    print("Items added successfully")
+
+def populate_packs():
+    cards = get_packs()
+    print("Got packs")
+
+    table = get_table_containing("Packs")
 
     for card in tqdm(cards):
         table.put_item(Item=card)
@@ -25,12 +43,15 @@ def get_cards():
     assert all("code" in card for card in cards)
     return cards
 
-def add_card(card: dict):
-    import boto3
+def get_packs():
+    
+    response = requests.get(ALL_PACKS_URL)
+    cards = response.json()
 
-
-
-
+    # Validate that every card has a "code"
+    assert all("id" in card for card in cards)
+    return cards
 
 if __name__ == "__main__":
-    main()
+    populate_player_cards()
+    populate_packs()
